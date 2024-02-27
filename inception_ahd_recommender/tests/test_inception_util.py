6fa334a6779025e5aec6daf9b8473c81adea438f
Franz Matthies <fmatthies@users.noreply.github.com>
2024-02-27 15:32:53 +0100

    Update test_inception_util.py
    import clarification

--- inception_ahd_recommender/tests/test_inception_util.py
+++ inception_ahd_recommender/tests/test_inception_util.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from ariadne.contrib.inception_util import *
+from inception_ahd_recommender.ariadne.contrib.inception_util import *
 
 from cassis import Cas, TypeSystem
 
